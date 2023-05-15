f2d58b2e6467c76d656bb5b1af2cf4971c30e19b
Jakub Nowosad <tupiszakaczki@gmail.com>
2023-05-15 18:53:52 +0200

    improves docs

--- R/patternogram.R
+++ R/patternogram.R
@@ -1,12 +1,12 @@
 #' Create a Patternogram or a Patternogram Cloud
 #'
-#' ...
+#' It creates a patternogram or a patternogram cloud. The function takes a raster object of class `SpatRaster` or a point vector object of class `sf` as input, and calculates the dissimilarity between values of pairs of points given the distances between them. The output of this function is a tibble of the patternogram class that can be visualized with the `plot()` and `autoplot()` functions.
 #'
 #' @param x A raster object of class SpatRaster (terra) or a point vector object of class sf (sf)
 #' @param cutoff Spatial distance up to which point pairs are included in patternogram estimates;
 #'   by default: a square root of the raster area
 #' @param width The width of subsequent distance intervals for which data point pairs are grouped for patternogram estimates
-#' @param dist_fun Distance measure used. This function uses the `philentropy::distance` function (run `philentropy::getDistMethods()` to find possible distance measures)
+#' @param dist_fun Distance measure used. This function uses the `philentropy::distance()` function (run `philentropy::getDistMethods()` to find possible distance measures)
 #' @param sample_size Only used when `x` is raster. Proportion of the cells inside of each region to be used in calculations. Value between 0 and 1. It is also possible to specify an integer larger than 1, in which case the specified number of cells of each region will be used in calculations.
 #' @param cloud Logical; if TRUE, calculate the patternogram cloud
 #' @param target Additional argument allowing to calculate separate estimates for different categories or ranges of values
