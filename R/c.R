99f5c5b87301d23f26cc9093930d7f7b83b6e8ef
Jakub Nowosad <tupiszakaczki@gmail.com>
2025-09-30 16:46:55 +0200

    adds docs to the c fun

--- R/c.R
+++ R/c.R
@@ -1,3 +1,21 @@
+#' Combine Patternogram Objects
+#'
+#' This function provides a method for `c()` that combines multiple
+#' `patternogram` objects into a single object, binding them by rows.
+#'
+#' @param ... One or more objects of class `patternogram`.
+#' @param ids Optional character vector of identifiers to assign to each input object. If `NULL`, identifiers are generated automatically.
+#'
+#' @return A `patternogram` object containing all input objects combined row-wise, with an added `id` column indicating their origin
+#'
+#' @examples
+#' r = terra::rast(system.file("ex/elev.tif", package = "terra"))
+#' r1 = r[r < 340, drop = FALSE]
+#' r2 = r[r >= 340, drop = FALSE]
+#' pr1 = patternogram(r1)
+#' pr2 = patternogram(r2)
+#' pr_all = c(pr1, pr2, ids = c("low", "high"))
+#' plot(pr_all)
 #' @export
 c.patternogram = function(..., ids = NULL) {
   # patternograms = c(list(x), list(...))
